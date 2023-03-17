6716e1d44bdc3f3036ab5dd82d4e6f4ee60c46e2
Tong Tang <pichip22@hotmail.com>
2023-03-17 19:43:00 -0400

    test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(a = `roller-skate`){
+    return `This Saturday, I want to ${a}!`;
+}
+function mondayWork(a = `go to the office`){
+    return `This Monday, I will ${a}.`;
+}
+function wrapAdjective(a){
+    return function result(b){
+        return `You are ${a}${b}${a}!`;
+    }
+}
